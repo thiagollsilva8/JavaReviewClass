08c2dc8ee5e29604501367733792fc81fe9acc0f
Thiago Silva <thiagollsilva8@gmail.com>
2023-06-18 22:08:24 -0400

    review 13

---
+++ Utils/Constants.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class Constants {
+
+    public static final String EXCEL_FILE_PATH = "C:\\Users\\thiag\\IdeaProjects\\JavaReviewBatch16\\Files\\HRMSEmployeesData.xlsx";
+    public static final String CONFIG_FILE_PATH="Files/config.properties";
+}
